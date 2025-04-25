a1cef1ff051b22f4acc48152cea6661cf4819cc0
Rylie Foster <54921469+ryliefoster@users.noreply.github.com>
2025-04-25 10:36:09 -0500

    Update myData.py

--- myData.py
+++ myData.py
@@ -1,6 +1,7 @@
 import searchTable as st
 
 entries = [st.Entry("Professor Messer CompTIA A+ Course","https://www.youtube.com/playlist?list=PLG49S3nxzAnnOmvg5UGVenB_qQgsh01uC","A youtube playlist of CompTIA A+ prep videos.",["CompTIA","IT"]),
-st.Entry("USDA Fern Page","https://www.fs.usda.gov/wildflowers/beauty/ferns/what.shtml","A USDA webpage describing ferns.",["ferns","plants"])]
+st.Entry("USDA Fern Page","https://www.fs.usda.gov/wildflowers/beauty/ferns/what.shtml","A USDA webpage describing ferns.",["ferns","plants"]),
+st.Entry("Wikipedia Fern Page","https://en.wikipedia.org/wiki/Fern","A Wikipedia webpage describing ferns.",["ferns","plants"])]
 
 myTable = st.SearchTable(entries)
